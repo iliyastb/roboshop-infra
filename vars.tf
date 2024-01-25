38afea6dfe61fc3c9c75433dbe100af60b133258
iliyastb <skiliyas300@gmail.com>
2024-01-25 21:22:01 +0530

    Update

--- vars.tf
+++ vars.tf
@@ -1,13 +1,13 @@
 variable "instances" {
   default = {
-    catalogue = {
-      name = "catalogue"
-      type = "t2.micro"
-    }
-    user = {
-      name = "user"
-      type = "t2.micro"
-    }
+#    catalogue = {
+#      name = "catalogue"
+#      type = "t2.micro"
+#    }
+#    user = {
+#      name = "user"
+#      type = "t2.micro"
+#    }
     frontend = {
       name = "frontend"
       type = "t2.micro"
