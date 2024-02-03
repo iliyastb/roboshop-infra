7042e31f8e576c6f50bc9e0b43c0752054d9e1c6
iliyastb <skiliyas300@gmail.com>
2024-02-03 14:38:20 +0530

    Update

--- ec2/main.tf
+++ ec2/main.tf
@@ -1,16 +1,32 @@
 data "aws_ami" "ami" {
   most_recent = true
-  name_regex  = "Centos-8-DevOps-Practice"
-  owners      = ["973714476881"]
+  name_regex  = "centos8-with-ansible"
+  owners      = ["860050401100"]
 }
 
-resource "aws_instance" "instances" {
+#973714476881
+
+resource "aws_spot_instance_request" "instances" {
   ami                    = data.aws_ami.ami.id
   instance_type          = var.instance_type
   vpc_security_group_ids = [var.sg_id]
 
   tags = {
     Name = var.component
+  }
+
+  provisioner "remote-exec" {
+
+    connection {
+      host = self.public_ip
+      user = "centos"
+      password = "DevOps321"
+    }
+
+    inline = [
+      "ansible-pull -i localhost -U https://github.com/iliyastb/roboshop-ansible.git roboshop.yml -e role_name=${var.component}",
+      "ls"
+    ]
   }
 }
 
@@ -19,9 +35,9 @@ variable "component" {}
 variable "sg_id" {}
 
 output "private_ip" {
-  value = aws_instance.instances.private_ip
+  value = aws_spot_instance_request.instances.private_ip
 }
 
 output "public_ip" {
-  value = aws_instance.instances.public_ip
+  value = aws_spot_instance_request.instances.public_ip
 }
