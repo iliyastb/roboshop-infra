586e6c614bd5fa15c0ab7d708b4ee8442ddf2fa5
iliyastb <skiliyas300@gmail.com>
2024-02-03 15:29:45 +0530

    Update

--- sample/main.tf
+++ sample/main.tf
@@ -1,7 +1,8 @@
-resource "aws_instance" "instances" {
+resource "aws_spot_instance_request" "instances" {
   ami                    = "ami-001be52dbdb73d993"
   instance_type          = "t3.micro"
   vpc_security_group_ids = ["sg-0f1959ab92bc12167"]
+  wait_for_fulfillment = true
 
   tags = {
     Name = "iliyas"
@@ -16,7 +17,7 @@ resource "aws_instance" "instances" {
     }
 
     inline = [
-      "ansible-pull -i localhost -U https://github.com/iliyastb/roboshop-ansible.git roboshop.yml -e role_name=frontend"
+      "ansible-pull -i localhost, -U https://github.com/iliyastb/roboshop-ansible.git roboshop.yml -e role_name=frontend"
     ]
   }
 }
