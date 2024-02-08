3f9bf9ace804239471b03e2f5d4a28710436477a
iliyastb <skiliyas300@gmail.com>
2024-02-08 20:12:37 +0530

    Update

--- ec2/main.tf
+++ ec2/main.tf
@@ -4,24 +4,15 @@ data "aws_ami" "ami" {
   owners      = ["860050401100"]
 }
 
-#973714476881
-
 resource "aws_instance" "instances" {
   ami                    = data.aws_ami.ami.id
   instance_type          = var.instance_type
   vpc_security_group_ids = [var.sg_id]
-#  wait_for_fulfillment = true
 
   tags = {
     Name = var.component
   }
 }
-
-#resource "aws_ec2_tag" "tags" {
-#  key         = "Name"
-#  resource_id = aws_instance.instances.id
-#  value       = var.component
-#}
 
 variable "instance_type" {}
 variable "component" {}
