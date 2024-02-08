64a9e737d51fe9f813ec011aa9b9fe77a0abd20e
iliyastb <skiliyas300@gmail.com>
2024-02-08 20:06:29 +0530

    Update

--- ec2/main.tf
+++ ec2/main.tf
@@ -6,31 +6,31 @@ data "aws_ami" "ami" {
 
 #973714476881
 
-resource "aws_spot_instance_request" "instances" {
+resource "aws_instance" "instances" {
   ami                    = data.aws_ami.ami.id
   instance_type          = var.instance_type
   vpc_security_group_ids = [var.sg_id]
-  wait_for_fulfillment = true
+#  wait_for_fulfillment = true
 
   tags = {
     Name = var.component
   }
 }
 
-resource "aws_ec2_tag" "tags" {
-  key         = "Name"
-  resource_id = aws_spot_instance_request.instances.spot_instance_id
-  value       = var.component
-}
+#resource "aws_ec2_tag" "tags" {
+#  key         = "Name"
+#  resource_id = aws_instance.instances.id
+#  value       = var.component
+#}
 
 variable "instance_type" {}
 variable "component" {}
 variable "sg_id" {}
 
 output "private_ip" {
-  value = aws_spot_instance_request.instances.private_ip
+  value = aws_instance.instances.private_ip
 }
 
 output "public_ip" {
-  value = aws_spot_instance_request.instances.public_ip
+  value = aws_instance.instances.public_ip
 }
