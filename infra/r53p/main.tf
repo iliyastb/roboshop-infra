191562c8d76f728cc99a23c4d97780f5d0083c11
iliyastb <skiliyas300@gmail.com>
2024-02-13 15:29:09 +0530

    Update

--- infra/r53p/main.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_route53_record" "pr53" {
-  name    = "devtb.online"
-  type    = "A"
-  ttl     = 30
-  records = [var.public_ip]
-  zone_id = "Z0519871SX8ZUH6ORUV5"
-}
-
-variable "public_ip" {}
