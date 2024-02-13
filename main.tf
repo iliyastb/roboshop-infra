acebb0486c6f692883af9a17a9f9e333294a9e1f
iliyastb <skiliyas300@gmail.com>
2024-02-13 18:59:00 +0530

    Update

--- main.tf
+++ main.tf
@@ -3,5 +3,5 @@ module "ec2" {
   for_each      = var.instances
   component     = each.value["name"]
   instance_type = each.value["type"]
-  public_ip = module.ec2[each.value["frontend"]].public_ip
+  public_ip = module.ec2["frontend"].public_ip
 }
