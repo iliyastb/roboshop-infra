34d532e264569880f7da56321740bd6c9216ea9b
iliyastb <skiliyas300@gmail.com>
2024-02-13 15:45:24 +0530

    Update

--- ec2/main.tf
+++ ec2/main.tf
@@ -1,27 +1,27 @@
-#resource "aws_security_group" "robo_sg" {
-#  name        = "robo_sg"
-#  description = "Allow TLS inbound traffic"
-#
-#  ingress {
-#    description      = "TLS from VPC"
-#    from_port        = 0
-#    to_port          = 0
-#    protocol         = "-1"
-#    cidr_blocks      = ["0.0.0.0/0"]
-#  }
-#
-#  egress {
-#    from_port        = 0
-#    to_port          = 0
-#    protocol         = "-1"
-#    cidr_blocks      = ["0.0.0.0/0"]
-#    ipv6_cidr_blocks = ["::/0"]
-#  }
-#
-#  tags = {
-#    Name = "robo_sg"
-#  }
-#}
+resource "aws_security_group" "sg" {
+  name        = "${var.component}-${var.env}-sg"
+  description = "Allow TLS inbound traffic"
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "${var.component}-${var.env}-sg"
+  }
+}
 
 data "aws_ami" "ami" {
   most_recent = true
@@ -72,3 +72,6 @@ resource "null_resource" "provisioner" {
 
 variable "instance_type" {}
 variable "component" {}
+variable "env" {
+  default = "dev"
+}
