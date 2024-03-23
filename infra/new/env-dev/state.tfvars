425e4d3835bf19c9a16e7a090b5175f6d4a89afa
iliyastb <skiliyas300@gmail.com>
2024-03-23 16:46:05 +0530

    Update

--- infra/new/env-dev/state.tfvars
+++
@@ -1,3 +0,0 @@
-#bucket = "devtb-state"
-#key = "dev/infra/terraform.tfstate"
-#region = "us-east-1"
--- infra/new/main.tf
+++
@@ -1,8 +0,0 @@
-#module "ec2" {
-#  source        = "./ec2"
-#  for_each      = var.instances
-#  component     = each.value["name"]
-#  instance_type = each.value["type"]
-#  env           = var.env
-#  monitor = try(each.value["monitor"], false)
-#}
