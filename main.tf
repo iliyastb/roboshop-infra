92f61bc4bae67edc46b28d5443d127d500e6e07b
iliyastb <skiliyas300@gmail.com>
2024-03-27 17:59:00 +0530

    Update

--- main.tf
+++ main.tf
@@ -12,23 +12,23 @@ module "vpc" {
   private_subnets = each.value["private_subnets"]
 }
 
-#module "docdb" {
-#  source = "git::https://github.com/iliyastb/tf-module-docdb.git"
-#  env = var.env
-#  tags = var.tags
-#
-#  for_each = var.docdb
-#  engine = each.value["engine"]
-#  engine_version = each.value["engine_version"]
-#  backup_retention_period = each.value["backup_retention_period"]
-#  preferred_backup_window = each.value["preferred_backup_window"]
-#  skip_final_snapshot = each.value["skip_final_snapshot"]
-#  instance_class = each.value["instance_class"]
-#  no_of_instances = each.value["no_of_instances"]
-#
-#  subnet_ids = local.db_subnets_ids
-#}
-#
+module "docdb" {
+  source = "git::https://github.com/iliyastb/tf-module-docdb.git"
+  env = var.env
+  tags = var.tags
+
+  for_each = var.docdb
+  engine = each.value["engine"]
+  engine_version = each.value["engine_version"]
+  backup_retention_period = each.value["backup_retention_period"]
+  preferred_backup_window = each.value["preferred_backup_window"]
+  skip_final_snapshot = each.value["skip_final_snapshot"]
+  instance_class = each.value["instance_class"]
+  no_of_instances = each.value["no_of_instances"]
+
+  subnet_ids = local.db_subnets_ids
+}
+
 #module "rds" {
 #  source = "git::https://github.com/iliyastb/tf-module-rds.git"
 #  env = var.env
@@ -74,6 +74,6 @@ module "vpc" {
 #  value = local.db_subnets_ids
 #}
 
-output "main" {
-  value = module.vpc
-}
+#output "main" {
+#  value = module.vpc
+#}
