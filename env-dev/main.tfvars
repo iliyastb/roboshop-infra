fb900a9a5903bff8141ace451b22f1bfe1379bfa
iliyastb <skiliyas300@gmail.com>
2024-03-30 18:02:07 +0530

    Update

--- env-dev/main.tfvars
+++ env-dev/main.tfvars
@@ -116,5 +116,9 @@ apps = {
   catalogue = {
     component = "catalogue"
     instance_type = "t3.nano"
+    desired_capacity   = 1
+    max_size           = 4
+    min_size           = 1
+    subnet_name = "app"
   }
 }
--- main.tf
+++ main.tf
@@ -91,6 +91,11 @@ module "app" {
   for_each = var.apps
   component = each.value["component"]
   instance_type = each.value["instance_type"]
+  desired_capacity = each.value["desired_capacity"]
+  max_size = each.value["max_size"]
+  min_size = each.value["min_size"]
+
+  subnets = lookup(local.subnet_ids, each.value["subnet_name"], null)
 }
 
 #output "main" {
