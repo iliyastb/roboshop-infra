009b542e46718ba80b1f846727c0c92a9aabc23f
iliyastb <skiliyas300@gmail.com>
2024-04-01 15:35:23 +0530

    Update

--- main.tf
+++ main.tf
@@ -69,7 +69,7 @@ module "vpc" {
 #
 #  subnet_ids = local.db_subnets_ids
 #}
-#
+
 module "alb" {
   source = "git::https://github.com/iliyastb/tf-module-alb.git"
   env = var.env
@@ -82,31 +82,31 @@ module "alb" {
 
   subnets = lookup(local.subnet_ids, each.value["subnet_name"], null)
 }
-#
-#module "app" {
-#  source = "git::https://github.com/iliyastb/tf-module-app.git"
-#  env = var.env
-#  tags = var.tags
-#
-#  vpc_id = module.vpc["main"].vpc_id
-#  bastion_cidr = var.bastion_cidr
-#
-#  for_each = var.apps
-#  component = each.value["component"]
-#  instance_type = each.value["instance_type"]
-#  desired_capacity = each.value["desired_capacity"]
-#  max_size = each.value["max_size"]
-#  min_size = each.value["min_size"]
-#  port = each.value["port"]
-#  allow_app_to = lookup(local.subnet_cidr, each.value["allow_app_to"], null)
-#
-#  subnets = lookup(local.subnet_ids, each.value["subnet_name"], null)
+
+module "app" {
+  source = "git::https://github.com/iliyastb/tf-module-app.git"
+  env = var.env
+  tags = var.tags
+
+  vpc_id = module.vpc["main"].vpc_id
+  bastion_cidr = var.bastion_cidr
+
+  for_each = var.apps
+  component = each.value["component"]
+  instance_type = each.value["instance_type"]
+  desired_capacity = each.value["desired_capacity"]
+  max_size = each.value["max_size"]
+  min_size = each.value["min_size"]
+  port = each.value["port"]
+  allow_app_to = lookup(local.subnet_cidr, each.value["allow_app_to"], null)
+
+  subnets = lookup(local.subnet_ids, each.value["subnet_name"], null)
+}
+
+#output "main" {
+#  value = local.db_subnets_ids
 #}
 #
-##output "main" {
-##  value = local.db_subnets_ids
-##}
-#
-##output "main" {
-##  value = module.vpc
-##}
+#output "main" {
+#  value = module.vpc
+#}
