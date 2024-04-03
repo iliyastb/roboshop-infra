b2fa24df764d619c0bc3dc0a0ad6556320f8eaa0
iliyastb <skiliyas300@gmail.com>
2024-04-03 21:58:22 +0530

    Update

--- main.tf
+++ main.tf
@@ -11,24 +11,24 @@ module "vpc" {
   private_subnets = each.value["private_subnets"]
 }
 
-module "docdb" {
-  source = "git::https://github.com/iliyastb/tf-module-docdb.git"
-  env    = var.env
-  tags   = var.tags
-
-  vpc_id = module.vpc["main"].vpc_id
-
-  for_each                = var.docdb
-  engine                  = each.value["engine"]
-  engine_version          = each.value["engine_version"]
-  backup_retention_period = each.value["backup_retention_period"]
-  preferred_backup_window = each.value["preferred_backup_window"]
-  skip_final_snapshot     = each.value["skip_final_snapshot"]
-  instance_class          = each.value["instance_class"]
-  no_of_instances         = each.value["no_of_instances"]
-  subnet_ids              = local.db_subnets_ids
-  allow_subnets           = lookup(local.subnet_cidr, each.value["allow_subnets"], null)
-}
+#module "docdb" {
+#  source = "git::https://github.com/iliyastb/tf-module-docdb.git"
+#  env    = var.env
+#  tags   = var.tags
+#
+#  vpc_id = module.vpc["main"].vpc_id
+#
+#  for_each                = var.docdb
+#  engine                  = each.value["engine"]
+#  engine_version          = each.value["engine_version"]
+#  backup_retention_period = each.value["backup_retention_period"]
+#  preferred_backup_window = each.value["preferred_backup_window"]
+#  skip_final_snapshot     = each.value["skip_final_snapshot"]
+#  instance_class          = each.value["instance_class"]
+#  no_of_instances         = each.value["no_of_instances"]
+#  subnet_ids              = local.db_subnets_ids
+#  allow_subnets           = lookup(local.subnet_cidr, each.value["allow_subnets"], null)
+#}
 
 #module "rds" {
 #  source = "git::https://github.com/iliyastb/tf-module-rds.git"
