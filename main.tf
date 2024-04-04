2303a07c7cffa7ce3d1a17bb6381fccf7ece9e5c
iliyastb <skiliyas300@gmail.com>
2024-04-04 16:00:05 +0530

    Update

--- main.tf
+++ main.tf
@@ -1,8 +1,7 @@
 module "vpc" {
-  source = "git::https://github.com/iliyastb/tf-module-vpc.git"
-  env    = var.env
-  tags   = var.tags
-
+  source          = "git::https://github.com/iliyastb/tf-module-vpc.git"
+  env             = var.env
+  tags            = var.tags
   default_vpc_id  = var.default_vpc_id
   default_rt      = var.default_rt
   for_each        = var.vpc
@@ -15,9 +14,7 @@ module "vpc" {
 #  source = "git::https://github.com/iliyastb/tf-module-docdb.git"
 #  env    = var.env
 #  tags   = var.tags
-#
 #  vpc_id = module.vpc["main"].vpc_id
-#
 #  for_each                = var.docdb
 #  engine                  = each.value["engine"]
 #  engine_version          = each.value["engine_version"]
@@ -34,7 +31,6 @@ module "vpc" {
 #  source = "git::https://github.com/iliyastb/tf-module-rds.git"
 #  env = var.env
 #  tags = var.tags
-#
 #  for_each = var.rds
 #  engine = each.value["engine"]
 #  engine_version = each.value["engine_version"]
@@ -49,7 +45,6 @@ module "vpc" {
 #  source = "git::https://github.com/iliyastb/tf-module-elasticache.git"
 #  env    = var.env
 #  tags   = var.tags
-#
 #  for_each        = var.elasticache
 #  engine          = each.value["engine"]
 #  engine_version  = each.value["engine_version"]
@@ -62,19 +57,16 @@ module "vpc" {
 #  source = "git::https://github.com/iliyastb/tf-module-rabbitmq.git"
 #  env = var.env
 #  tags = var.tags
-#
 #  for_each = var.rabbitmq
 #  instance_type = each.value["instance_type"]
 #  subnet_ids = local.db_subnets_ids
 #}
 
 module "alb" {
-  source = "git::https://github.com/iliyastb/tf-module-alb.git"
-  env    = var.env
-  tags   = var.tags
-
-  vpc_id = module.vpc["main"].vpc_id
-
+  source             = "git::https://github.com/iliyastb/tf-module-alb.git"
+  env                = var.env
+  tags               = var.tags
+  vpc_id             = module.vpc["main"].vpc_id
   for_each           = var.alb
   name               = each.value["name"]
   internal           = each.value["internal"]
@@ -89,21 +81,16 @@ module "app" {
   tags             = var.tags
   bastion_cidr     = var.bastion_cidr
   dns_domain       = var.dns_domain
-
-  vpc_id = module.vpc["main"].vpc_id
-
-  for_each          = var.apps
-  component         = each.value["component"]
-  instance_type     = each.value["instance_type"]
-  desired_capacity  = each.value["desired_capacity"]
-  max_size          = each.value["max_size"]
-  min_size          = each.value["min_size"]
-  port              = each.value["port"]
-  listener_priority = each.value["listener_priority"]
-  subnets           = lookup(local.subnet_ids, each.value["subnet_name"], null)
-  allow_app_to      = lookup(local.subnet_cidr, each.value["allow_app_to"], null)
-  alb_dns_name      = lookup(lookup(lookup(module.alb, each.value["alb"], null), "alb", null), "dns_name", null)
-  listener_arn      = lookup(lookup(lookup(module.alb, each.value["alb"], null), "listener", null), "arn", null)
+  vpc_id           = module.vpc["main"].vpc_id
+  for_each         = var.apps
+  component        = each.value["component"]
+  instance_type    = each.value["instance_type"]
+  desired_capacity = each.value["desired_capacity"]
+  max_size         = each.value["max_size"]
+  min_size         = each.value["min_size"]
+  port             = each.value["port"]
+  subnets          = lookup(local.subnet_ids, each.value["subnet_name"], null)
+  allow_app_to     = lookup(local.subnet_cidr, each.value["allow_app_to"], null)
 }
 
 output "alb" {
