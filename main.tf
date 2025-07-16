87c90980cd321b9474c4eefdf4ab5c6121682f45
iliyastb <skiliyas300@gmail.com>
2025-07-16 16:39:28 +0530

    Update

--- main.tf
+++ main.tf
@@ -10,65 +10,65 @@ module "vpc" {
   private_subnets = each.value["private_subnets"]
 }
 
-module "docdb" {
-  source                  = "git::https://github.com/iliyastb/tf-module-docdb.git"
-  env                     = var.env
-  tags                    = var.tags
-  vpc_id                  = module.vpc["main"].vpc_id
-  for_each                = var.docdb
-  engine                  = each.value["engine"]
-  engine_version          = each.value["engine_version"]
-  backup_retention_period = each.value["backup_retention_period"]
-  preferred_backup_window = each.value["preferred_backup_window"]
-  skip_final_snapshot     = each.value["skip_final_snapshot"]
-  instance_class          = each.value["instance_class"]
-  no_of_instances         = each.value["no_of_instances"]
-  subnet_ids              = local.db_subnets_ids
-  allow_subnets           = lookup(local.subnet_cidr, each.value["allow_subnets"], null)
-}
-
-module "rds" {
-  source                  = "git::https://github.com/iliyastb/tf-module-rds.git"
-  env                     = var.env
-  tags                    = var.tags
-  vpc_id                  = module.vpc["main"].vpc_id
-  for_each                = var.rds
-  engine                  = each.value["engine"]
-  engine_version          = each.value["engine_version"]
-  backup_retention_period = each.value["backup_retention_period"]
-  preferred_backup_window = each.value["preferred_backup_window"]
-  instance_class          = each.value["instance_class"]
-  no_of_instances         = each.value["no_of_instances"]
-  subnet_ids              = local.db_subnets_ids
-  allow_subnets           = lookup(local.subnet_cidr, each.value["allow_subnets"], null)
-}
-
-module "elasticache" {
-  source          = "git::https://github.com/iliyastb/tf-module-elasticache.git"
-  env             = var.env
-  tags            = var.tags
-  vpc_id          = module.vpc["main"].vpc_id
-  for_each        = var.elasticache
-  engine          = each.value["engine"]
-  engine_version  = each.value["engine_version"]
-  num_cache_nodes = each.value["num_cache_nodes"]
-  node_type       = each.value["node_type"]
-  subnet_ids      = local.db_subnets_ids
-  allow_subnets   = lookup(local.subnet_cidr, each.value["allow_subnets"], null)
-}
-
-module "rabbitmq" {
-  source        = "git::https://github.com/iliyastb/tf-module-rabbitmq.git"
-  env           = var.env
-  tags          = var.tags
-  bastion_cidr  = var.bastion_cidr
-  dns_domain    = var.dns_domain
-  vpc_id        = module.vpc["main"].vpc_id
-  for_each      = var.rabbitmq
-  instance_type = each.value["instance_type"]
-  subnet_ids    = local.db_subnets_ids
-  allow_subnets = lookup(local.subnet_cidr, each.value["allow_subnets"], null)
-}
+#module "docdb" {
+#  source                  = "git::https://github.com/iliyastb/tf-module-docdb.git"
+#  env                     = var.env
+#  tags                    = var.tags
+#  vpc_id                  = module.vpc["main"].vpc_id
+#  for_each                = var.docdb
+#  engine                  = each.value["engine"]
+#  engine_version          = each.value["engine_version"]
+#  backup_retention_period = each.value["backup_retention_period"]
+#  preferred_backup_window = each.value["preferred_backup_window"]
+#  skip_final_snapshot     = each.value["skip_final_snapshot"]
+#  instance_class          = each.value["instance_class"]
+#  no_of_instances         = each.value["no_of_instances"]
+#  subnet_ids              = local.db_subnets_ids
+#  allow_subnets           = lookup(local.subnet_cidr, each.value["allow_subnets"], null)
+#}
+#
+#module "rds" {
+#  source                  = "git::https://github.com/iliyastb/tf-module-rds.git"
+#  env                     = var.env
+#  tags                    = var.tags
+#  vpc_id                  = module.vpc["main"].vpc_id
+#  for_each                = var.rds
+#  engine                  = each.value["engine"]
+#  engine_version          = each.value["engine_version"]
+#  backup_retention_period = each.value["backup_retention_period"]
+#  preferred_backup_window = each.value["preferred_backup_window"]
+#  instance_class          = each.value["instance_class"]
+#  no_of_instances         = each.value["no_of_instances"]
+#  subnet_ids              = local.db_subnets_ids
+#  allow_subnets           = lookup(local.subnet_cidr, each.value["allow_subnets"], null)
+#}
+#
+#module "elasticache" {
+#  source          = "git::https://github.com/iliyastb/tf-module-elasticache.git"
+#  env             = var.env
+#  tags            = var.tags
+#  vpc_id          = module.vpc["main"].vpc_id
+#  for_each        = var.elasticache
+#  engine          = each.value["engine"]
+#  engine_version  = each.value["engine_version"]
+#  num_cache_nodes = each.value["num_cache_nodes"]
+#  node_type       = each.value["node_type"]
+#  subnet_ids      = local.db_subnets_ids
+#  allow_subnets   = lookup(local.subnet_cidr, each.value["allow_subnets"], null)
+#}
+#
+#module "rabbitmq" {
+#  source        = "git::https://github.com/iliyastb/tf-module-rabbitmq.git"
+#  env           = var.env
+#  tags          = var.tags
+#  bastion_cidr  = var.bastion_cidr
+#  dns_domain    = var.dns_domain
+#  vpc_id        = module.vpc["main"].vpc_id
+#  for_each      = var.rabbitmq
+#  instance_type = each.value["instance_type"]
+#  subnet_ids    = local.db_subnets_ids
+#  allow_subnets = lookup(local.subnet_cidr, each.value["allow_subnets"], null)
+#}
 
 module "alb" {
   source             = "git::https://github.com/iliyastb/tf-module-alb.git"
